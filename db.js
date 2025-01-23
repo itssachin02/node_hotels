c385f7198a3e63c28eda523f8005edaed66ece74
Sachin G <sachin@gmail.com>
2025-01-23 21:58:31 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb://localhost:27017/hotels'
+//const mongoURL = 'mongodb://localhost:27017/hotels'
+const mongoURL = 'mongodb+srv://sunny773841:V8toXOo4XzosstHA@cluster0.aneav.mongodb.net'
 
 // set up mongodb connection
 mongoose.connect(mongoURL, {
